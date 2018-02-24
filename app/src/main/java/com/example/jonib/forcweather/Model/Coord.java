d431d7d1f09aa2109da612480587c6e68396ef1b
GitJonibek <jonibeknorboev@gmail.com>
2018-02-24 11:49:35 -0800

    Working with Models

--- app/src/main/java/com/example/jonib/forcweather/Model/Coord.java
+++ app/src/main/java/com/example/jonib/forcweather/Model/Coord.java
@@ -5,4 +5,29 @@ package com.example.jonib.forcweather.Model;
  */
 
 public class Coord {
+
+    private double lat;
+    private double lon;
+
+    public Coord(double lat, double lon){
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
 }
---
+++ app/src/main/java/com/example/jonib/forcweather/Model/Weather.java
@@ -0,0 +1,53 @@
+package com.example.jonib.forcweather.Model;
+
+/**
+ * Created by jonib on 2/24/2018.
+ */
+
+public class Weather {
+
+    private int id;
+    private String main;
+    private String description;
+    private String icon;
+
+    public Weather(int id, String main, String description, String icon){
+        this.id = id;
+        this.main = main;
+        this.description = description;
+        this.icon = icon;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+}
